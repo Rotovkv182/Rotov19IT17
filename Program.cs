a56707a18c4df500583991dec0a894938e5c0a58
Rotov <rotovkv182@gmail.com>
2020-02-04 18:24:32 +0300

    UPD LAB

--- Program.cs
+++ Program.cs
@@ -8,41 +8,42 @@ namespace FLAB
 {
     class Program
     {
-        // Task: R минимально, R кратно 21 ,если такого числа нет вывести -1 
+        /* Task: R минимально, R кратно 21, произведение 2х различных элементов последовательности, 
+           если такого числа нет вывести -1 */ 
         static void Main(string[] args)
         {
-            Random rand = new Random();     //случайные числа
-            int[] mas = new int[1000];     //массив
-            for (int i = 0; i < 1000; i++) //цикл от 0 до 10000 заполнен случайными числами
+            Random rand = new Random();     //рандомные числа
+            int R = -1;                     
+            int check = 0;                  //проверка
+            int enumer;                     //перебор
+
+            for (int i = 0; i < 1000; i++)
             {
-                mas[i] = rand.Next(0, 10000);
-                Console.WriteLine(mas[i]);
-            }
-            int R = mas[0];
-            Console.WriteLine("Minimal Number is ");
-            for (int i = 0; i < mas.Length; i++)     //Поиск минимального числа и присвоение его R
-            {
-                if (mas[i] < R)
+                enumer = rand.Next(0, 10000);  //внесение рандомных чмсел в переменную
+                if (enumer < check)            //проверка на то, что результат цикла будет минимальный
                 {
-                    R = mas[i];
+                    check = enumer;
+                }
+                int a = 0;
+                int b = 0;
+                if ((enumer % 7 == 0) && (a < enumer))
+                {
+                    a = enumer;
+                }
+                if ((enumer % 3 == 0) && (b < enumer))
+                {
+                    b = enumer;
+                }
+                if ((check < R) && (check % 21 == 0) && (check == (a * b)))    
+                 /* Переменная проверяется на условие: check - минимально, кратно 21 и является произведением
+                 2х различных элементов последовательности. 
+                 Если check удовлетворяет условию, то присваевается R, если нет, R остается -1.*/
+                {
+                    R = check;
                 }
             }
             Console.WriteLine(R);
-            Console.WriteLine("Condition: ");     /* проверка на условие если "R кратно 21" и 
-            является произведением двух случайных чисел массива если соответствует, 
-            то выводится R, если нет, выводится -1 */
-            int a = mas[new Random().Next(0, mas.Length)];
-            int b = mas[new Random().Next(0, mas.Length)];
-            int c = a * b;
-            if ((R % 21 == 0) && (R == c && (a - b <= 8 || b - a <= 8 )))
-            {
-                Console.WriteLine(R);
-            }
-            else
-            {
-                Console.WriteLine("-1");
-            }
         }
     }
 }
-
+ 
